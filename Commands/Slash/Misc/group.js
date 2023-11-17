c9a04f6210a522af6f052c14be8b9e32002576e0
DucKyWay <niji2549@gmail.com>
2023-11-18 02:04:00 +0700

    test group.js

---
+++ Commands/Slash/Misc/group.js
@@ -0,0 +1,66 @@
+const { ApplicationCommandType, ChannelType, PermissionFlagsBits,InteractionType } = require("discord.js");
+const { Bot } = require("../../../handlers/Client");
+
+module.exports = {
+    name: "group",
+    description: 'Group management.',
+    userPermissions: PermissionFlagsBits.SendMessages,
+    botPermissions: PermissionFlagsBits.Administrator,
+    category: "Misc",
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'create',
+            description: 'create new group.',
+            type: 3,
+            required: false,
+        },
+        {
+            name: 'adduser',
+            description: 'add user to your group.',
+            type: 3,
+            required: false,
+        },
+        {
+            name: 'rename',
+            description: 'rename your group name.',
+            type: 3,
+            required: false,
+        },
+    ],
+
+    run: async (client, interaction,guild) => {
+        const optionNames = ['create', 'adduser', 'rename'];
+        let chosenOption = null;
+    
+        for (const optionName of optionNames) {
+            const currentOption = interaction.options.get(optionName);
+            if (currentOption) {
+                chosenOption = currentOption;
+                break;
+            }
+        }
+    
+        if (!chosenOption) {
+            return interaction.reply('Please choose an option.');
+        }
+
+        if (chosenOption.name == 'create') {
+            return "Create new channel.";
+            
+        }
+        else if (chosenOption.name == 'adduser') {
+            return "Add user to group."
+        }
+        else if (chosenOption.name == 'rename') {
+            return "Rename your group."
+        } else {
+            return client.sendEmbed(interaction,{
+                title: 'Error 404',
+                description: 'please contact your teacher for fix it!',
+                color: '#ED4245',
+            });
+        }
+    },
+    
+};
