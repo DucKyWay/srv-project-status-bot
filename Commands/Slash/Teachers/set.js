a07522d542640db3ae51987ea51be13302d1ff24
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-17 23:26:36 +0700

    Update set.js

--- Commands/Slash/Teachers/set.js
+++ Commands/Slash/Teachers/set.js
@@ -13,19 +13,19 @@ module.exports = {
             name: 'setcategory',
             description: 'set category channel in server for student create channel teammate group.',
             type: 3,
-            required: true,
+            required: false,
         },
         {
             name: 'addcatoegory',
             description: 'add category channel in server for student create channel teammate group.',
             type: 3,
-            required: true,
+            required: false,
         },
         {
             name: 'changecatoegoryname',
             description: 'change category name channel in server for student create channel teammate group.',
             type: 3,
-            required: true,
+            required: false,
         }
     ],
 
