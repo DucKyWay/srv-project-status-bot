f559a70235152e9f0760fcf519378918e4daa73d
DucKyWay <niji2549@gmail.com>
2023-11-19 01:39:50 +0700

    mongo boommmm

--- handlers/Client.js
+++ handlers/Client.js
@@ -9,7 +9,7 @@ const {
   Colors,
 } = require("discord.js");
 const { title } = require("process");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 class Bot extends Client {
   constructor() {
@@ -135,19 +135,19 @@ class Bot extends Client {
     };
   }
 
-  connectToDatabase() {
-    const mongoURI = process.env.MONGODB_URI;
-    mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+  // connectToDatabase() {
+  //   const mongoURI = process.env.MONGODB_URI;
+  //   mongoose.connect(mongoURI, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true
+  //   });
 
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    db.once('open', () => {
-      console.log("Connected successfully to MongoDB");
-    });
-  }
+  //   const db = mongoose.connection;
+  //   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  //   db.once('open', () => {
+  //     console.log("Connected successfully to MongoDB");
+  //   });
+  // }
 }
 
 module.exports = { Bot };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Bot } = require("./handlers/Client");
 const { TOKEN } = require("./settings/config");
 const client = new Bot();
 
-client.connectToDatabase(); // Add this line
+// client.connectToDatabase(); // Add this line
 
 module.exports = client;
 client.build(TOKEN);
