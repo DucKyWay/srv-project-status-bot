9a17b8917c4069ca5a6820d9ce84d1f18d4a37fc
DucKyWay <niji2549@gmail.com>
2023-11-19 16:07:34 +0700

    connect mongo on productionn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { Bot } = require("./handlers/Client");
 const { TOKEN } = require("./settings/config");
 const client = new Bot();
+const mongoose = require('mongoose');
 
-// client.connectToDatabase(); // Add this line
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('Connected to Mongodb.'));
 
 module.exports = client;
 client.build(TOKEN);
