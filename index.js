32a377b898302dff60c0915f80221063ca6b7c8f
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-19 17:02:43 +0700

    catch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { TOKEN } = require("./settings/config");
 const client = new Bot();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('Connected to Mongodb.'));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('Connected to Mongodb.')).catch((e)=> {console.log(e)});
 
 module.exports = client;
 client.build(TOKEN);
