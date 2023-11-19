d2c3ee9f7d42e8393350def4b33f09588820cc6e
1REDfriend <tongtongkonthai.34930@gmail.com>
2023-11-19 23:29:36 +0700

    Update group.js

--- Commands/Slash/Misc/group.js
+++ Commands/Slash/Misc/group.js
@@ -1,6 +1,6 @@
-const { ApplicationCommandType, ChannelType, PermissionFlagsBits,InteractionType } = require("discord.js");
+const { ApplicationCommandType, ChannelType, PermissionFlagsBits, InteractionType } = require("discord.js");
 const { Bot } = require("../../../handlers/Client");
-
+const fs = require('fs');
 module.exports = {
     name: "group",
     description: 'Group management.',
@@ -29,83 +29,100 @@ module.exports = {
         },
     ],
 
-    run: async (client, interaction,guild) => {
-        const optionNames = ['create', 'adduser', 'rename'];
-        let chosenOption = null;
-    
-        for (const optionName of optionNames) {
-            const currentOption = interaction.options.get(optionName);
-            if (currentOption) {
-                chosenOption = currentOption;
-                break;
+    run: async (client, interaction, guild) => {
+        fs.readFile('./data/data.json', 'utf8', async (err, data) => {
+            if (err) {
+                console.error('Error reading data.json:', err);
+                return;
             }
-        }
-    
-        if (!chosenOption) {
-            return interaction.reply('Please choose an option.');
-        }
 
-        if (chosenOption.name == 'create') {
-            const group_name = interaction.options.get('create').value;
-            const role_name = interaction.options.get('create').value;
-            const role_color = '#ffeb00';
-            const member = interaction.member;
+            const guildId = interaction.guild.id;
 
-            try {
-                const new_group_name = await interaction.guild.channels.create({
-                    name: group_name,
-                    parent: "1174916854553260082",
-                    type: ChannelType.GuildText,
-                });
+            const jsonData = JSON.parse(data);
 
-                const new_role = await interaction.guild.roles.create({
-                    name: role_name,
-                    color: role_color
-                });
-
-                await member.roles.add(interaction.guild.roles.cache.find(r => r.name === role_name));
-                await interaction.reply(`Channel <#${new_group_name.id}> and role <@&${new_role.id}> are created!`);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply('Failed to create channel and role.');
+            if (!jsonData[guildId]) {
+                jsonData[guildId] = {};
             }
-        }
-        else if (chosenOption.name == 'adduser') {
-            // const user_tagged = interaction.options.getUser('adduser');
-            // const add_role = interaction.options.getString('some role'); 
 
-            // if (!user_tagged || !add_role) {
-            //     return interaction.reply({ content: "Please specify a user and a role.", ephemeral: true });
-            // }
-            // const role = interaction.guild.roles.cache.find(r => r.name === add_role);
+            const categoryId = jsonData[guildId].categoryId
 
-            // if (!role) {
-            //     return interaction.reply({ content: "Role not found.", ephemeral: true });
-            // }
-            // const member = interaction.guild.members.cache.get(user_tagged.id);
+            const optionNames = ['create', 'adduser', 'rename'];
+            let chosenOption = null;
 
-            // if (!member) {
-            //     return interaction.reply({ content: "Member not found.", ephemeral: true });
-            // }
+            for (const optionName of optionNames) {
+                const currentOption = interaction.options.get(optionName);
+                if (currentOption) {
+                    chosenOption = currentOption;
+                    break;
+                }
+            }
 
-            // try {
-            //     await member.roles.add(role);
-            //     await interaction.reply({ content: `Role ${role.name} added to ${member.displayName}.`, ephemeral: true });
-            // } catch (error) {
-            //     console.error(error);
-            //     await interaction.reply({ content: "Failed to add role.", ephemeral: true });
-            // }
-            return "Add user to your group."
-        }
-        else if (chosenOption.name == 'rename') {
-            return "Rename your group."
-        } else {
-            return client.sendEmbed(interaction,{
-                title: 'Error 404',
-                description: 'please contact your teacher for fix it!',
-                color: '#ED4245',
-            });
-        }
-    },
-    
+            if (!chosenOption) {
+                return interaction.reply('Please choose an option.');
+            }
+
+            if (chosenOption.name == 'create') {
+                const group_name = interaction.options.get('create').value;
+                const role_name = interaction.options.get('create').value;
+                const role_color = '#ffeb00';
+                const member = interaction.member;
+
+                try {
+                    const new_group_name = await interaction.guild.channels.create({
+                        name: group_name,
+                        parent: categoryId.toString(),
+                        type: ChannelType.GuildText,
+                    });
+
+                    const new_role = await interaction.guild.roles.create({
+                        name: role_name,
+                        color: role_color,
+                    });
+
+                    await member.roles.add(interaction.guild.roles.cache.find(r => r.name === role_name));
+
+                    await interaction.reply(`Channel <#${new_group_name.id}> and role <@&${new_role.id}> are created!`);
+                } catch (error) {
+                    console.error(error);
+                    await interaction.reply('Failed to create channel and role.');
+                }
+            }
+            else if (chosenOption.name == 'adduser') {
+                // const user_tagged = interaction.options.getUser('adduser');
+                // const add_role = interaction.options.getString('some role'); 
+
+                // if (!user_tagged || !add_role) {
+                //     return interaction.reply({ content: "Please specify a user and a role.", ephemeral: true });
+                // }
+                // const role = interaction.guild.roles.cache.find(r => r.name === add_role);
+
+                // if (!role) {
+                //     return interaction.reply({ content: "Role not found.", ephemeral: true });
+                // }
+                // const member = interaction.guild.members.cache.get(user_tagged.id);
+
+                // if (!member) {
+                //     return interaction.reply({ content: "Member not found.", ephemeral: true });
+                // }
+
+                // try {
+                //     await member.roles.add(role);
+                //     await interaction.reply({ content: `Role ${role.name} added to ${member.displayName}.`, ephemeral: true });
+                // } catch (error) {
+                //     console.error(error);
+                //     await interaction.reply({ content: "Failed to add role.", ephemeral: true });
+                // }
+                // return "Add user to your group."
+            }
+            else if (chosenOption.name == 'rename') {
+                return "Rename your group."
+            } else {
+                return client.sendEmbed(interaction, {
+                    title: 'Error 404',
+                    description: 'please contact your teacher for fix it!',
+                    color: '#ED4245',
+                });
+            }
+        })
+    }
 };
